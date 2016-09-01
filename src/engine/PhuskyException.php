2be07ca34724f253956e2d5bc195bf721874ef86
Antonio Giliberto <giliweb.it@gmail.com>
2016-09-01 09:37:59 +0200

    test2

--- src/engine/PhuskyException.php
+++ src/engine/PhuskyException.php
@@ -1,4 +1,11 @@
 <?php namespace giliweb\phusky;
 class PhuskyException extends \Exception {
 
+	const UNKOWN_ERROR = "Unkown error: %s";
+
+
+	public function __construct($code, array $data = []){
+		$message = substr_count($code, '%') != count($data) ? $code : vprintf($code, $data);
+		parent::__construct($message);
+	}
 }
